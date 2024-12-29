6b10ce4e37d4efcec9a63d618f45b8eb4dbc4325
JoãoDevel <joaovitormatosfontes15@gmail.com>
2024-12-29 12:56:28 +0100

    updates section

--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -3,35 +3,37 @@ import Video from "../../assets/video/videoOficial.webm";
 
 function Section() {
   return (
-    <div className="w-full flex">
-      <div className="w-[50%] relative">
-        <div className="relative w-full h-full pb-[56.25%] overflow-hidden">
-          <video
-            className="absolute top-0 left-0 w-full h-full z-10"
-            autoPlay
-            loop
-            muted
-            playsInline
-          >
-            <source src={Video} type="video/webm" />
-            Your browser does not support the video tag.
-          </video>
+    <div className="w-full">
+      <div className="w-full h-full flex">
+        <div className="w-[50%] relative">
+          <div className="relative w-full h-full pb-[56.25%] overflow-hidden">
+            <video
+              className="absolute top-0 left-0 w-full h-full z-10"
+              autoPlay
+              loop
+              muted
+              playsInline
+            >
+              <source src={Video} type="video/webm" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-12 items-center justify-center bg-black max-w-[50%] px-36 py-16 ">
-        <h2 className="text-white text-center font-jost text-3xl font-light">
-          Imagina morder una rebanada de pizza tan deliciosa que haga que tu
-          paladar dance de placer. En Sabor Divino, cada ingrediente se elige
-          con cariño, cada masa se prepara con perfección y cada horno es una
-          promesa de un sabor inigualable.
-        </h2>
-        <div className="flex gap-12">
-          <button className="bg-[#00B03B] text-white w-[142px] h-[33px] font-semibold font-poppins rounded">
-            Hacer Pedido
-          </button>
-          <button className="bg-[#D2210D] text-white w-[142px] h-[33px] font-semibold font-poppins rounded">
-            Hacer Pedido
-          </button>
+        <div className="flex flex-col gap-12 items-center justify-center bg-black max-w-[50%] px-36 py-16 ">
+          <h2 className="text-white text-center font-jost text-3xl font-light">
+            Imagina morder una rebanada de pizza tan deliciosa que haga que tu
+            paladar dance de placer. En Sabor Divino, cada ingrediente se elige
+            con cariño, cada masa se prepara con perfección y cada horno es una
+            promesa de un sabor inigualable.
+          </h2>
+          <div className="flex gap-12">
+            <button className="bg-[#00B03B] text-white w-[142px] h-[33px] font-semibold font-poppins rounded">
+              Hacer Pedido
+            </button>
+            <button className="bg-[#D2210D] text-white w-[142px] h-[33px] font-semibold font-poppins rounded">
+              Hacer Pedido
+            </button>
+          </div>
         </div>
       </div>
     </div>
