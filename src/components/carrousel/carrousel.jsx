f200710091be5c306d522545870fc691014727c9
miqueias199808@outlook.com <miqueias199808@outlook.com>
2024-12-29 12:31:07 +0100

    updapte 1.0.1 miqueias

--- src/components/carrousel/carrousel.jsx
+++ src/components/carrousel/carrousel.jsx
@@ -1,18 +1,18 @@
 function Carrousel() {
   return (
-    <div className="px-5 w-full bg-black overflow-hidden font-jockey ">
+    <div className="px-5 py-4 w-full bg-black overflow-hidden font-jockey ">
       <span className="text-white text-[1.7rem] inline-block whitespace-nowrap animate-move font-poppins">
-        Sabor Divino <strong className="text-green-400">2025</strong> &nbsp;
-        &nbsp; Sabor Divino <strong className="text-green-400">2025</strong>{" "}
+        Sabor Divino <strong className="text-[#e24b3b]">2025</strong> &nbsp;
+        &nbsp; Sabor Divino <strong className="text-[#e24b3b]">2025</strong>{" "}
         &nbsp; &nbsp; Sabor Divino{" "}
-        <strong className="text-green-400">2025</strong> &nbsp; &nbsp; Sabor
-        Divino <strong className="text-green-400">2025</strong> &nbsp; &nbsp;
-        Sabor Divino <strong className="text-green-400">2025</strong> &nbsp;
-        &nbsp; Sabor Divino <strong className="text-green-400">2025</strong>{" "}
+        <strong className="text-[#e24b3b]">2025</strong> &nbsp; &nbsp; Sabor
+        Divino <strong className="text-[#e24b3b]">2025</strong> &nbsp; &nbsp;
+        Sabor Divino <strong className="text-[#e24b3b]">2025</strong> &nbsp;
+        &nbsp; Sabor Divino <strong className="text-[#e24b3b]">2025</strong>{" "}
         &nbsp; &nbsp; Sabor Divino{" "}
-        <strong className="text-green-400">2025</strong> &nbsp; &nbsp; Sabor
+        <strong className="text-[#e24b3b]">2025</strong> &nbsp; &nbsp; Sabor
         Divino 2025&nbsp; &nbsp; Sabor Divino{" "}
-        <strong className="text-green-400">2025</strong>
+        <strong className="text-[#e24b3b]">2025</strong>
       </span>
     </div>
   );
--- src/components/mainContent/mainContent.jsx
+++ src/components/mainContent/mainContent.jsx
@@ -1,7 +1,22 @@
+import PizzaMain from "../../assets/image/pizzaMain.jpg";
+
 function MainContent() {
   return (
-    <div className="">
-      <p>teste</p>
+    <div className="w-full">
+      <div className="flex text-center items-center w-full py-10">
+        <section className="w-full px-5">
+          <p className=" text-center text-[1.84rem] font-jost">
+            Imagina morder una rebanada de pizza <br /> tan deliciosa que haga
+            que tu paladar <br /> dance de placer. En Sabor Divino, <br />
+            cada ingrediente se elige con cariño, <br /> cada masa se prepara
+            con perfección y <br /> cada horno es una promesa de <br /> un sabor
+            inigualable.
+          </p>
+        </section>
+        <section className="w-full px-5 ">
+          <img src={PizzaMain} alt="" className="w-[75%] rounded-lg" />
+        </section>
+      </div>
     </div>
   );
 }
