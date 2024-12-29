15b6c4a34a98ccf15ca88813f793b49b6492f777
JoãoDevel <joaovitormatosfontes15@gmail.com>
2024-12-29 11:59:19 +0100

    feat: add new section

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,44 @@
+import { BsTwitterX } from "react-icons/bs";
+import Logo from "../../assets/image/logopizza.png";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FiYoutube } from "react-icons/fi";
+
 function Footer() {
   return (
-    <div>
-      <p>teste</p>
+    <div className="px-8 py-14 flex border-t-2">
+      <div className="flex  flex-col items-center mr-[15rem]">
+        <img src={Logo} alt="" className="w-[100px] mb-4" />
+        <div className="flex gap-3 items-center">
+          <BsTwitterX className="w-[24px] h-[24px] cursor-pointer" />
+          <FaInstagram className="w-[24px] h-[24px] cursor-pointer" />
+          <FiYoutube className="w-[24px] h-[24px] cursor-pointer" />
+          <FaLinkedin className="w-[24px] h-[24px] cursor-pointer" />
+        </div>
+      </div>
+      <div className="mr-[15rem]">
+        <h2 className="font-poppins font-bold text-xl mb-6">Tipos de Pizza</h2>
+        <ul className="flex flex-col gap-4 text-md font-poppins font-light">
+          <li className="cursor-pointer">Pizza Margherita</li>
+          <li className="cursor-pointer">Pizza Pepperoni</li>
+          <li className="cursor-pointer">Pizza Hawaiana</li>
+          <li className="cursor-pointer">Pizza Quattro Formaggi</li>
+          <li className="cursor-pointer">Pizza Vegetariana</li>
+          <li className="cursor-pointer">Pizza Barbacoa</li>
+          <li className="cursor-pointer">Pizza Marinera</li>
+        </ul>
+      </div>
+      <div>
+        <h2 className="font-poppins font-bold text-xl mb-6">Explorar</h2>
+        <ul className="flex flex-col gap-4 text-md font-poppins font-light">
+          <li className="cursor-pointer">Menú</li>
+          <li className="cursor-pointer">Ofertas Especiales</li>
+          <li className="cursor-pointer">Contacto</li>
+          <li className="cursor-pointer">Ubicación</li>
+          <li className="cursor-pointer">Reseñas</li>
+          <li className="cursor-pointer">Pedidos Online</li>
+          <li className="cursor-pointer">Sobre Nosotros</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,25 +3,33 @@ import Pizza from "../../assets/image/pizzaHeader.png";
 
 function Header() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <header className="flex items-center justify-between px-8 py-4 w-full mb-2">
-        <img src={Logo} alt="" className="w-[90px] cursor-pointer" />
+    <div className="flex flex-col justify-center items-center py-4 mb-3">
+      <header className="flex items-center justify-between px-8  w-full mb-4">
+        <img src={Logo} alt="" className="w-[110px] cursor-pointer" />
         <ul className="flex gap-14 text-lg font-bold">
-          <li className="cursor-pointer">Home</li>
-          <li className="cursor-pointer">Menu</li>
-          <li className="cursor-pointer">Contacto</li>
-          <li className="cursor-pointer">Nosotros</li>
+          <li className="pb-1 border-b-2 border-transparent hover:border-black cursor-pointer">
+            Home
+          </li>
+          <li className="pb-1 border-b-2 border-transparent hover:border-black cursor-pointer">
+            Menu
+          </li>
+          <li className="pb-1 border-b-2 border-transparent hover:border-black cursor-pointer">
+            Contacto
+          </li>
+          <li className="pb-1 border-b-2 border-transparent hover:border-black cursor-pointer">
+            Nosotros
+          </li>
         </ul>
 
-        <button className="bg-[#D2210D] text-white w-[138px] h-[33px] font-bold">
+        <button className="bg-[#D2210D] text-white w-[138px] h-[33px] font-bold rounded">
           Hacer Pedido
         </button>
       </header>
-      <img src={Pizza} alt="" />
-      <h1 className="text-[10.5rem] text-[#CF210D] font-bold font-jockey">
-        SABOR DIVINO
+      <img src={Pizza} alt="" className="w-[500px]" />
+      <h1 className="text-[16.5rem] text-[#CF210D] font-bold font-jockey">
+        Sabor Divino
       </h1>
-      <p className="text-lg font-sm ">
+      <p className="text-xl font-md font-poppins ">
         Porque la vida sabe mejor con una pizza divina.
       </p>
     </div>
