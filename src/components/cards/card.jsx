0946acadb85caa97e438a44de0f70ff2da6309c2
miqueias199808@outlook.com <miqueias199808@outlook.com>
2025-01-05 11:05:14 +0100

    update test 2

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -41,25 +41,6 @@ CardContent.propTypes = {
 
 function Card() {
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col w-full justify-center items-center px-8">
-      <div className="mb-1">
-        <span className="font-jost text-[6rem] md:text-[10rem] lg:text-[14rem] xl:text-[22rem] text-black tracking-wide block md:inline-block">
-          Best-
-        </span>
-        <span className="font-jost text-[2rem] md:text-[2.5rem] lg:text-[3rem] xl:text-[3.5rem] text-[#0B7F3A] block md:inline-block">
-          Sellers.
-        </span>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 justify-items-center p-4">
-        <CardContent
-          image="/public/card9.png"
-          text="Marguerita"
-          text2="Salsa de tomate, Mozzarella, Albahaca, Aceite de oliva."
-          title="Pizza Marguerita"
-          price={8.2}
-        />
-=======
     <ScrollEffect>
       <div className="flex flex-col w-full justify-center items-center px-8">
         <div className="mb-1">
@@ -85,7 +66,6 @@ function Card() {
             title="Pizza Marguerita"
             price={8.2}
           />
->>>>>>> miqueias
 
           <CardContent
             image="/public/card10.png"
